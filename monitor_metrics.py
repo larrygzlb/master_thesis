b887ae3046e764860b0726ef281d871630d4e793
unknown <larrygzlb@gmail.com>
2024-04-19 11:39:04 +0200

    add tensorboard intro

---
+++ monitor_metrics.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio
+
+generate_output_for_trainset(model, dataset):
+    
+    return trainset_outputs_list
+
+generate_output_for_validset():
+    
+    return validset_outputs_list
+compute_psnr():
+    
+    
+    return psnr
+
+compute_ssim():
+    
+    return ssim
